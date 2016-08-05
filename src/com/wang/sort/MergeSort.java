7df6b3aaa41223c534893250e5795f16ad30cec6
vwangjinpeng <vwangjinpeng@163.com>
2016-08-05 08:08:37 +0800

    Add Merge

--- src/com/wang/sort/MergeSort.java
+++ src/com/wang/sort/MergeSort.java
@@ -2,31 +2,44 @@ package com.wang.sort;
 
 public class MergeSort {
 	static int[] ListA = {12,32,31,14,52,45};
+	
 	public static void main(String[] args) {
-		MSort(ListA,0,5);
+		int[] Temp=new int[ListA.length];
+		int First=0;
+		int Last=ListA.length-1;
+		MSort(ListA,Temp,First,Last);
+		for(int item : ListA)
+			System.out.print(item+",");
 	}
-	//合并将两个有序序列（[low,mid],[mid+1,high]）合并
-	public static void MSort(int[] ListA, int First, int Last){
-		int Mid = (int)((ListA.length-1)/2);
+	public static void MSort(int[] ListB, int[] Temp, int First, int Last){
+		if(First!=Last){
+			int Mid=(int)((ListB.length-1)/2);
+			MSort(ListB, Temp, First, Mid);
+			MSort(ListB, Temp, Mid+1, Last);
+			Merge(ListB, Temp, First, Mid, Last);
+		}
+	}
+	//合并将两个有序序列（[first,mid],[mid+1,last]）合并
+	public static void Merge(int[] ListC, int[] Temp, int First, int Mid, int Last){
+		Mid=(int)((ListC.length-1)/2);
 		int i=First;
 		int j=Mid+1;
-		int[] NewList = new int[ListA.length];
-		int NewData=0;
+		int TempData=0;
 		while(i<=Mid&&j<=Last){
-			if(ListA[i]<ListA[j]){
-				NewList[NewData++]=ListA[i++];
+			if(ListC[i]<ListC[j]){
+				Temp[TempData++]=ListC[i++];
 			}
 			else{
-				NewList[NewData++]=ListA[j++];
+				Temp[TempData++]=ListC[j++];
 			}
 		}
 		while(i<=Mid){
-			NewList[NewData++]=ListA[i++];
+			Temp[TempData++]=ListC[i++];
 		}
 		while(j<=Last){
-			NewList[NewData++]=ListA[j++];
+			Temp[TempData++]=ListC[j++];
 		}
-		for(i=0;i<ListA.length;i++)
-			ListA[i]=NewList[i];
+		for(i=0;i<ListC.length;i++)
+			ListC[i]=Temp[i];
 	}
 }

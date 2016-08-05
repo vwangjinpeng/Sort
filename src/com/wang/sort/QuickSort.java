f49e0bd582d8f260d3c6d1e0ad2b7a24eaee3622
pc <pc@pc-PC>
2016-08-05 18:05:03 +0800

    AddQuick

---
+++ src/com/wang/sort/QuickSort.java
@@ -0,0 +1,26 @@
+package com.wang.sort;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		int[] ListA = new int[]{12,32,11,43,23,12,34};
+
+
+	}
+	public static void QSort(int[] ListB, int Left, int Right){
+		int m = Left;
+		int n = Right;
+		int Key = ListB[Left];
+		while(m<n)
+		{
+			while(ListB[m]>Key){
+				Swap(ListB[m],ListB[Left]);
+			}
+		}
+	}
+	public static void Swap(int a, int b){
+		int temp=a;
+		b=a;
+		a=temp;
+	}
+}
